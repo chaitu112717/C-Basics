82db7e43b52a2ed3ea11d384b202268fbf56931e
chaitu112717 <92074922+chaitu112717@users.noreply.github.com>
2021-10-23 10:59:07 +0530

    Add files via upload

---
+++ vairable decleration.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int age;
+	float CGPA;
+	char initial;
+	
+	//initialzation
+	age= 21;
+	CGPA= 8.5;
+	initial='C';
+	
+	//output
+	printf("age of the student is %d\n", age);
+	printf("CGPA of the student is %f\n", CGPA);
+	printf("student surname start with %c", initial);
+}
+	
+
