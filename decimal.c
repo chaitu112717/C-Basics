d5445e6df4d0fc64be55d4074ddf2766398477b8
chaitu112717 <92074922+chaitu112717@users.noreply.github.com>
2021-10-23 10:58:46 +0530

    Add files via upload

---
+++ decimal.c
@@ -0,0 +1,17 @@
+/* adding two decimal values */
+#include<stdio.h>
+void main()
+{
+	// varaible declecration
+	float number1, number2, sum;
+	
+	// initialzation
+	number1 = 8.112717;
+	number2 = 7.112717;
+	
+	// calculation '
+	sum = number1+number2;
+	
+	// output
+	printf("sum of %f and %f is %f",number1,number2,sum);
+}
