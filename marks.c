9695854e7d95752da8906ba53f7efbd4973440f5
chaitu112717 <92074922+chaitu112717@users.noreply.github.com>
2021-10-28 10:50:58 +0530

    Add files via upload

---
+++ marks.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int phy, che, mat, eng, tot;
+	
+	//Initialization
+	phy = 90;
+	che = 70;
+	mat = 97;
+	eng = 60;
+	
+	 //calculation
+	 tot = phy+che+mat+eng;
+	 
+	 //output
+	 printf("Marks in physics: %d\n", phy);
+	 printf("Marks in chemistry: %d\n", che);
+	 printf("Marks in maths: %d\n", mat);
+	 printf("Marks in english: %d\n", eng);
+	 printf("Total marks: %d", tot);
+	 
+ 	
+}
+
