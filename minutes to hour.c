25a294e94ba5dc720f11e509a0cab7950c46ce63
chaitu112717 <92074922+chaitu112717@users.noreply.github.com>
2021-11-12 10:10:31 +0530

    Add files via upload

---
+++ minutes to hour.c
@@ -0,0 +1,14 @@
+// convert the given minutes into hours & minutes
+
+#include<stdio.h>
+void main()
+{
+	int minutes,hours,totaltime;
+	printf("enter total minutes");
+	scanf("%d", &minutes);
+	printf("%d hour(s) %d minutes(s)",minutes/60, minutes%60);
+	
+	
+	
+	
+}
